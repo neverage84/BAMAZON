82dbb2055a64a84e82135e6b35f625290bdd1686
Noelle Everage <neverage84@yahoo.com>
2019-11-13 16:43:51 -0800

    Created products table

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -3,3 +3,11 @@ DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
+
+CREATE TABLE products(
+ item_id INTEGER(11) AUTO_INCREMENT,
+ product_name VARCHAR(30),
+ department_name VARCHAR(50),
+ price FLOAT(10,2),
+ stock_quantity INTEGER(11)
+);
