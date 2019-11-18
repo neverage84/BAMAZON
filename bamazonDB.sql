ac6b95a62eea5719f5fbe9a5f260d7e40004b000
Noelle Everage <neverage84@yahoo.com>
2019-11-17 20:12:13 -0800

    Added mysql to create departments table

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -9,7 +9,8 @@ CREATE TABLE products(
  product_name VARCHAR(30),
  department_name VARCHAR(50),
  price FLOAT(10,2),
- stock_quantity INTEGER(11)
+ stock_quantity INTEGER(11),
+ PRIMARY KEY (item_id)
 );
 
 INSERT INTO products( product_name, department_name, price, stock_quantity)
@@ -44,3 +45,11 @@ VALUES ("Dishsoap", "Cleaning Products", 5.50, 80);
 
 INSERT INTO products( product_name, department_name, price, stock_quantity)
 VALUES ("Paper Towels", "Cleaning Products", 7.75, 15);
+
+CREATE TABLE departments(
+department_id INTEGER(11) AUTO_INCREMENT,
+department_name VARCHAR(50),
+over_head_costs INTEGER(11),
+PRIMARY KEY (department_id)
+);
+
